0ea41492755c16b0ff0d7e59ed0453bc4dc137f0
stephenpalgeorge <stephenpalgeorge@gmail.com>
2021-04-27 16:39:03 +0100

    add layout and fetch menu

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+  export async function load() {
+    const url = "http://localhost:1337/menus/6087f2b0e98743662275e912";
+    const res = await fetch(url);
+
+    if (res.ok) {
+      return {
+        props: {
+          menu: await res.json()
+        }
+      };
+    }
+
+    return {
+      status: res.status,
+      error: new Error(`Could not load ${url}`)
+    };
+  }
+</script>
+
+<script>
+  export let menu;
+</script>
+
+<nav />
+<slot />
